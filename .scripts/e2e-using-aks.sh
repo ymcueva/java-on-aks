8d2d80c33311194d786a9a8c030f3fe1f13a2e7e
Asir Vedamuthu Selvasingh <asirveda@microsoft.com>
2019-12-01 20:49:17 -0800

    some minor changes

--- .scripts/e2e-using-aks.sh
+++ .scripts/e2e-using-aks.sh
@@ -62,13 +62,69 @@ kompose convert
 
 kubectl apply -f config-deployment.yaml
 kubectl apply -f registry-deployment.yaml
-kubectl apply -f registry-service.yaml
+# kubectl apply -f registry-service.yaml
 kubectl apply -f gateway-deployment.yaml
-kubectl apply -f gateway-service.yaml
+# kubectl apply -f gateway-service.yaml
 kubectl apply -f account-service-deployment.yaml
 kubectl apply -f auth-service-deployment.yaml
 kubectl apply -f statistics-service-deployment.yaml
 kubectl apply -f notification-service-deployment.yaml
 
-kubectl expose deployment gateway --type=LoadBalancer --name=gateway
+kubectl expose deployment config --type=LoadBalancer --port=8888 --target-port=8888
+kubectl expose deployment gateway --type=LoadBalancer --port=80 --target-port=4000
+kubectl expose deployment registry --type=LoadBalancer --port=8761 --target-port=8761
+kubectl expose deployment account-service --type=LoadBalancer --port=6000 --target-port=6000
+kubectl expose deployment auth-service --type=LoadBalancer --port=5000 --target-port=5000
 
+
+# few additional commands
+
+kubectl get pods
+kubectl get services
+kubectl get svc gateway
+kubectl delete pod [pod-name]
+
+http://.../actuator
+
+# https://codefresh.io/kubernetes-tutorial/kubernetes-cheat-sheet/
+
+
+# Azure Monitor - Kusto Query
+let ContainerIdList = KubePodInventory
+| where ContainerName contains  "/statistics-service"
+| where ClusterId =~ '/subscriptions/343129b6-cc9a-49f8-8b05-fbebbc594bd1/resourceGroups/e2e-java-microservices-to-aks/providers/Microsoft.ContainerService/managedClusters/aks4piggymetrics'
+| distinct ContainerID;
+ContainerLog
+| where ContainerID in (ContainerIdList)
+| project LogEntrySource, LogEntry, TimeGenerated, Computer, Image, Name, ContainerID
+| order by TimeGenerated desc
+| render table
+
+
+# Code Less Insights using Application Insights
+
+# https://github.com/microsoft/Application-Insights-K8s-Codeless-Attach/blob/master/SETUP.md
+
+# Install helm
+# https://helm.sh/docs/intro/install/
+
+brew install helm
+
+# Install dos2unix
+
+brew install dos2unix
+
+# Download code-less insights software
+
+# https://github.com/microsoft/Application-Insights-K8s-Codeless-Attach/releases
+
+# Execute init.sh from the release in a linux terminal
+
+source init.sh
+
+# Edit values.yaml
+# fill up AKS target namespace and Application Insights instrumentation key
+
+# install code-less insights
+
+helm install ./helm-version.tgz -f values.yaml
